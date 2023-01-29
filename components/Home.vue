e5c4208d27f8941653a910d2a025472aaf4a34c5
lucassantoli <lucas.santana349@gmail.com>
2023-01-29 17:58:09 -0300

    Initial commit

---
+++ components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home container">
+    asdasd
+    <sdg />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss">
+  .home {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Home />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style lang="scss">
+@import "@/styles/commons.scss";
+
+html {
+  background-color: $background;
+  color: $text;
+}
+</style>
